90f95f580edfcbddd9b34de3fe32dc79128e858a
Jan Troler <jan.troler@galtys.com>
2022-11-14 23:02:12 +0000

    opt_out will be made mandatory

--- addons/email_template/res_partner.py
+++ addons/email_template/res_partner.py
@@ -28,9 +28,12 @@ class res_partner(osv.osv):
     _inherit = 'res.partner'
 
     _columns = {
-        'opt_out': fields.boolean('Opt-Out',
-            help="If opt-out is checked, this contact has refused to receive emails for mass mailing and marketing campaign. "
-                    "Filter 'Available for Mass Mailing' allows users to filter the partners when performing mass mailing."),
+
+        'opt_out': fields.selection( [('optin','Opt In'), ('optout','Opt Out')], string="Opt In/Out"),
+        'opt_out_bkup':fields.boolean('opout2'),
+        #'opt_out': fields.boolean('Opt-Out',
+        #    help="If opt-out is checked, this contact has refused to receive emails for mass mailing and marketing campaign. "
+        #            "Filter 'Available for Mass Mailing' allows users to filter the partners when performing mass mailing."),
     }
 
     _defaults = {
